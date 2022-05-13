6263c7481d5da61dfa7a70320230d00d7ea25327
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2022-05-13 12:09:02 +0100

    Add: changed app title

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import './App.css';
 import MovieCard from './Components/MovieCard'
 import SearchIcon from './search.svg';
 
-
-const movie1 = {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-}
-
 const API_URL = 'http://www.omdbapi.com?apikey=f136a832'
 
 const App= () => {
@@ -27,7 +18,7 @@ const App= () => {
   }
 
   useEffect(() => {
-    searchMovies('spiderman');
+    searchMovies('Batman');
   }, []);
 
 
