4c6c1ee5b659ed97c4e807f9ec413ceab62820da
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2022-10-13 12:35:51 +0100

    Add: deploy build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import MovieCard from './Components/MovieCard'
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=f136a832'
+const API_URL = 'http://www.omdbapi.com?apikey=a4a35b01'
 
 const App= () => {
   const [movies, setMovies] = useState([]);
